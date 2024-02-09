afe991c0b1e06a53add773a2123489aa8c064ca8
bhoomi159 <159517996+bhoomi159@users.noreply.github.com>
2024-02-09 13:05:01 +0530

    Add files via upload

---
+++ 20240208-122329.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+       int a,b,c:";
+    cout << " enter the value of a and b : ";
+    cin>>a >>b ;
+    c=a+b;
+    cout<<"addition: "<<c;
+    c=a-b;
+    cout<<"\nsubstruction:"<<c;
+    c=a*b;
+    cout<"\nmultipletion:"<<c;
+    
+    return 0;
+}
